e9a24a992eb6ecd22c7928f3ce00d78845342fe0
Dvicto <artempr15@gmail.com>
2019-12-04 23:57:50 +0300

    123

--- php/day02/ex02/magnifying_glass.php
+++ php/day02/ex02/magnifying_glass.php
@@ -1,25 +1,27 @@
 #!/usr/bin/php
-<?php
-	if ($argc > 1)
-	{
-		$str = file_get_contents($argv[1]);
-		$titles = '#<a .+title(?<titles>=".+)".+/a>#';
-		$inside = '#<a.*?(?<inside>>[\w\s]+<).*?/a>#';
-		$result1 = preg_match_all($titles, $str, $match1);
-		$result2 = preg_match_all($inside, $str, $match2);
-		//$match = array($match1, $match2);
-		$i = 0;
-		while ($match1["titles"][$i])
-		{
-			$str = str_replace($match1["titles"][$i], strtoupper($match1["titles"][$i]), ($str));
-			$i++;
-		}
-		$i = 0;
-		while ($match2["inside"][$i])
-		{
-			$str = str_replace($match2["inside"][$i], strtoupper($match2["inside"][$i]), ($str));
-			$i++;
-		}
-		echo $str.PHP_EOL;
-	}
+<?PHP
+
+function replace_title($match){
+    $str = 'title="'.strtoupper($match[1]).'"';
+    return $str;
+}
+
+function replace($match){
+    $str = strtoupper($match[0]);
+    return $str;
+}
+
+function replace_link($match){
+    $str = preg_replace_callback('/>.*</siU', "replace", $match[0]);
+    return $str;
+}
+
+if ($argc == 1)
+    return ;
+$content = file_get_contents($argv[1]);
+
+$content = preg_replace_callback('/title="(.*?)"/', "replace_title", $content);
+
+$content = preg_replace_callback('/<a [^>]+.*<\/a>/siU', "replace_link", $content);
+print $content;
 ?>
---
+++ php/day02/ex04/photos.php
@@ -0,0 +1,36 @@
+#!/usr/bin/php
+<?php
+if ($argc != 2)
+    return ;
+$url = $argv[1];
+$dir = basename($url);
+mkdir("{$dir}", 0777, true);
+if (!($fd = curl_init($url))){
+    echo "Connection error";
+    rmdir($dir);
+    return ;
+}
+
+curl_setopt($fd, CURLOPT_RETURNTRANSFER, 1);
+$str = curl_exec($fd);
+curl_close($fd);
+preg_match_all ("/<[iI][mM][gG].*?[Ss][Rr][Cc].*?=\"(.*?)\"/", $str, $matches);
+foreach($matches[1] as &$value) {
+    if ($value[0] == "/")
+        $value = $url . $value;
+}
+
+foreach ($matches[1] as $value)
+{
+    $img_name = basename($value);
+    $path_name = $dir."/".$img_name;
+    $filed = fopen($path_name, 'wb');
+    $fd = curl_init($value);
+    curl_setopt($fd, CURLOPT_FILE, $filed);
+    curl_setopt($fd, CURLOPT_HEADER, 0);
+    curl_exec($fd);
+    curl_close($fd);
+    fclose($filed);
+}
+
+?>
