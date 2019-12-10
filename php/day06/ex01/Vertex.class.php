1bd1e69d1ec7481f85a709b339cf87f3baf2b6bd
Artem <artempr15@gmail.com>
2019-12-10 23:36:40 +0300

    weqwe

--- php/day06/ex01/Vertex.class.php
+++ php/day06/ex01/Vertex.class.php
@@ -1,1 +1,78 @@
 <?php
+
+	require_once '../ex00/Color.class.php';
+    class Vertex{
+    	private $x;
+    	private $y;
+    	private $z;
+    	private $w;
+    	private $color;
+		public static $verbose = FALSE;
+
+		public function getX(){
+			return $this->x;
+		}
+
+	    public function getY()
+	    {
+		    return $this->y;
+	    }
+
+	    public function getZ()
+	    {
+		    return $this->z;
+	    }
+
+	    public function getW()
+	    {
+		    return $this->w;
+	    }
+	    public function getColor()
+	    {
+			return $this->color;
+	    }
+	    public function __Construct(array $coords = array('x' => 0.0, 'y' => 0.0, 'z' => 0.0, 'w' => 0.0)){
+			if (isset($coords['color'])){
+				$this->color = $coords['color'];
+			}
+			else {
+				$this->color = new Color();
+			}
+			if (isset($coords['x']) && isset($coords['y']) && isset($coords['z'])){
+				$this->x = $coords['x'];
+				$this->y = $coords['y'];
+				$this->z = $coords['z'];
+			}
+			else{
+				$this->x = 0.0;
+				$this->y = 0.0;
+				$this->z = 0.0;
+			}
+			if (isset($coords['w'])){
+				$this->w = $coords['w'];
+			}
+			else{
+				$this->w = 1.0;
+			}
+			if (self::$verbose == true){
+				echo $this.' constructed'.PHP_EOL;
+			}
+	    }
+	    public function doc(){
+			echo file_get_contents('./Vertex.doc.txt').PHP_EOL;
+	    }
+
+	    public function __Destruct(){
+			if (self::$verbose == true){
+				echo $this.' destructed'.PHP_EOL;
+			}
+	    }
+
+	    public function __toString()
+	    {
+		    if (!self::$verbose)
+			    return sprintf("Vertex( x: %4.2f, y: %4.2f, z:%4.2f, w:%4.2f )", $this->x, $this->y, $this->z, $this->w);
+		    else
+			    return sprintf("Vertex( x: %4.2f, y: %4.2f, z:%4.2f, w:%4.2f, %s )", $this->x, $this->y, $this->z, $this->w, $this->color);
+	    }
+    }
---
+++ php/day06/ex02/Vector.class.php
@@ -0,0 +1,110 @@
+<?php
+
+	require_once '../ex01/Vertex.class.php';
+	class Vector{
+		private $x;
+		private $y;
+		private $z;
+		private $w = 0;
+		static $verbose = false;
+		public function getX()
+		{
+			return $this->x;
+		}
+
+		public function getY()
+		{
+			return $this->y;
+		}
+
+		public function getZ()
+		{
+			return $this->z;
+		}
+
+		public function getW()
+		{
+			return $this->w;
+		}
+
+		public function __construct($array)
+		{
+			if (isset($array['dest']) && $array['dest'] instanceof Vertex)
+			{
+				if (isset($array['orig']) && $array['orig'] instanceof Vertex)
+					$orig = new Vertex(array('x' => $array['orig']->getX(), 'y' => $array['orig']->getY(), 'z' => $array['orig']->getZ()));
+				else
+					$orig = new Vertex(array('x' => 0, 'y' => 0, 'z' => 0));
+				$this->x = $array['dest']->getX() - $orig->getX();
+				$this->y = $array['dest']->getY() - $orig->getY();
+				$this->z = $array['dest']->getZ() - $orig->getZ();
+				$this->w = 0;
+			}
+			if (self::$verbose == TRUE)
+				echo $this.' constructed'.PHP_EOL;
+			return ;
+		}
+		public function __Destruct()
+		{
+			if (self::$verbose == TRUE)
+				echo $this.' destructed'.PHP_EOL;
+			return ;
+		}
+		function __toString()
+		{
+			return (sprintf("Vector( x:%0.2f, y:%0.2f, z:%0.2f, w:%0.2f )",$this->x, $this->y, $this->z, $this->w));
+		}
+		public static function doc()
+		{
+			return file_get_contents("./Vector.doc.txt");
+		}
+		public function magnitude()
+		{
+			return (float)sqrt(($this->x * $this->x) + ($this->y * $this->y) + ($this->z * $this->z));
+		}
+		public function normalize()
+		{
+			$l = $this->magnitude();
+			if ($l == 1)
+				return clone $this;
+			return new Vector(array('dest' => new Vertex(array
+			('x' => $this->x / $l, 'y' => $this->y / $l, 'z' => $this->z / $l))));
+		}
+		public function add(Vector $rhs)
+		{
+			return new Vector(array('dest' => new Vertex(array('x' => $this->x + $rhs->x,
+				'y' => $this->y + $rhs->y, 'z' => $this->z + $rhs->z))));
+		}
+		public function sub(Vector $rhs)
+		{
+			return new Vector(array('dest' => new Vertex(array('x' => $this->x - $rhs->x,
+				'y' => $this->y - $rhs->y, 'z' => $this->z - $rhs->z))));
+		}
+		public function opposite()
+		{
+			return new Vector(array('dest' => new Vertex
+			(array('x' => $this->x * -1, 'y' => $this->y * -1, 'z' => $this->z * -1))));
+		}
+		public function scalarProduct($k)
+		{
+			return new Vector(array('dest' => new Vertex
+			(array('x' => $this->x * $k, 'y' => $this->y * $k, 'z' => $this->z * $k))));
+		}
+		public function dotProduct(Vector $rhs)
+		{
+			return (float)(($this->x * $rhs->x) + ($this->y * $rhs->y) + ($this->z * $rhs->z));
+		}
+		public function crossProduct(Vector $rhs)
+		{
+			return new Vector(array('dest' => new Vertex(array(
+				'x' => $this->y * $rhs->getZ() - $this->z * $rhs->getY(),
+				'y' => $this->z * $rhs->getX() - $this->x * $rhs->getZ(),
+				'z' => $this->x * $rhs->getY() - $this->y * $rhs->getX()))));
+		}
+		public function cos(Vector $rhs)
+		{
+			return ($this->dotProduct($rhs) / sqrt
+				((($this->x * $this->x) + ($this->y * $this->y) + ($this->z * $this->z)) *
+					(($rhs->x * $rhs->x) + ($rhs->y * $rhs->y) + ($rhs->z * $rhs->z))));
+		}
+	}
